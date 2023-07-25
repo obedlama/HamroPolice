67a28ea8d8ed82d95a8c36a69894b9266db568f4
obedlama <obedlama6@gmail.com>
2023-07-25 18:29:59 +0545

    bcrypt password hashing

--- client/src/pages/register/index.js
+++ client/src/pages/register/index.js
@@ -1,73 +1,98 @@
-import React from 'react';
+import React from 'react'
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import Link from 'next/link'
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import { useRouter } from 'next/navigation'
+import { Button, message } from 'antd';
 
 
-const SignupSchema = Yup.object().shape({
-  fullName: Yup.string()
-  .min(2, 'Too Short!')
-  .max(50, 'Too Long!')
-  .required('Required'),
-password: Yup.string()
-.min(5, 'Password Too Short!')
-.required('Required'),
-confirmpassword: Yup.string()
-.min(5, 'Password Too Short!')
-.required('Required')
-.oneOf([Yup.ref('password'), null], 'Passwords must match'),
-email: Yup.string().email('Invalid email').required('Required'),
-});
+const Register = () => {
 
-const handleResgister = async (values) => {
-  const {confirmpassword, ...formFields} = values
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(formFields)
-};
-fetch('http://localhost:4000/register', requestOptions)
-}
+  const [messageApi, contextHolder] = message.useMessage();
+  const router = useRouter()
 
-export const Register = () => (
-  <div className='Register'>
-    <h1>Signup</h1>
-    <Formik
-      initialValues={{
-        fullName: '',
-        phoneNumber: '',
-        email: '',
-        password: ''
-      }}
-      validationSchema={SignupSchema}
-      onSubmit={values => {
-        handleResgister(values)
-        // console.log(values);
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          Full Name: <Field name="fullName" placeholder="Full Name"/>
-          {errors.fullName && touched.fullName ? (
-            <div>{errors.fullName}</div>
-          ) : null} <br/>
-          PHone Number:<Field name="phoneNumber" placeholder="Phone Number"/>
-          {errors.phoneNumber && touched.phoneNumber ? (
-            <div>{errors.phoneNumber}</div>
-          ) : null} <br/>
-         Email:  <Field name="email" type="email" placeholder="Email"/>
-          {errors.email && touched.email ? <div>{errors.email}</div> : null} <br/>
-          Password:  <Field name="password" type="password" placeholder="Password" />
-          {errors.password && touched.password ? <div>{errors.password}</div> : null} <br/>
-          Confirm Password: <Field name="confirmpassword" type="password" placeholder="Confirm Password"/>
-             {errors.confirmpassword && touched.confirmpassword ? <div>{errors.confirmpassword}</div> : null} <br/>
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
 
-    Already have an account?  <Link href="/login">Login</Link> instead
-  </div>
-);
+    const SignupSchema = Yup.object().shape({
 
-export default Register
+      
+      fullName: Yup.string()
+          .min(2, 'Too Short!')
+          .max(50, 'Too Long!')
+          .required('Required'),
+        password: Yup.string()
+        .min(5, 'Password Too Short!')
+        .required('Required'),
+        confirmPassword: Yup.string()
+        .min(5, 'Password Too Short!')
+        .required('Required')
+        .oneOf([Yup.ref('password'), null], 'Passwords must match'),
+        email: Yup.string().email('Invalid email').required('Required'),
+      });
+
+
+      const handleRegister = async(values) => {
+        const {confirmPassword, ...formFields }= values
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(formFields)
+      };
+      const res = await fetch('http://localhost:4000/register',requestOptions)
+      const data = await res.json()
+      if(data) {
+        messageApi.info(data.msg);
+        router.push('/login')
+      }
+      }
+    return(
+        <>
+        {contextHolder}
+        <Header/>
+      <div className='container'> 
+      <div className="app--login">
+        <h2>Sign up</h2>
+        <Formik
+         initialValues={{
+            fullName: '',
+            email: '',
+            phoneNumber: '',
+            password:'',
+            confirmPassword: ''
+         }}
+         validationSchema={SignupSchema}
+         onSubmit={values => {
+          handleRegister(values)
+         }}
+       >
+         {({ errors, touched }) => (
+           <Form>
+             <Field name="fullName" placeholder="Full Name"/>
+             {errors.fullName && touched.fullName ? (
+               <div>{errors.fullName}</div>
+             ) : null}
+             <Field name="email" type="email" placeholder="Email"/>
+             {errors.email && touched.email ? <div>{errors.email}</div> : null}
+             <Field name="password" type="password" placeholder="Password"/>
+             {errors.password && touched.password ? <div>{errors.password}</div> : null}
+             <Field name="confirmPassword" type="password" placeholder="Confirm Password"/>
+             {errors.confirmPassword && touched.confirmPassword ? <div>{errors.confirmPassword}</div> : null}
+             <Field name="phoneNumber" type="text"  placeholder="Phone Number"/>
+             {errors.phoneNumber && touched.phoneNumber ? <div>{errors.phoneNumber}</div> : null}
+             <button type="submit">Signup</button>
+           </Form>
+         )}
+       </Formik>
+        <p>Already have an account? <a href="/login">Sign in</a></p>
+      </div>
+      </div>
+      <Footer/>
+      <div>
+      
+
+      </div>
+      </>
+    )
+  }
+
+export default Register;
