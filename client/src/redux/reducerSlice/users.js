3249294677fb6b9b63a7c3d1cccc8a323fd353c4
obedlama <obedlama6@gmail.com>
2023-08-03 15:38:02 +0545

    README updated

--- client/src/redux/reducerSlice/users.js
+++ client/src/redux/reducerSlice/users.js
@@ -1,23 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-<<<<<<< HEAD
 export const initialState = {
    token:'',
    userDetails: {},
    isLoggedIn: false
-=======
-
-export const initialState = {
-  token:'',
-  userDetails: {},
-  isLoggedIn: false
->>>>>>> d9460774f9f69c34ae1b41b8cfeec5ef3a82b3e5
 };
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {
-<<<<<<< HEAD
              setUserDetails(state,actions) {
         
                 const {token, userDetails} = actions.payload
@@ -32,20 +23,6 @@ const usersSlice = createSlice({
             },
     },
     })
-=======
-      setUserDetails(state, actions) {
-      debugger;
-      const {token, userDetails} =  actions.payload
-      return {
-        ...state,
-        token,
-        userDetails,
-        isLoggedIn: true
-      }
-      },
-    },
-  })
->>>>>>> d9460774f9f69c34ae1b41b8cfeec5ef3a82b3e5
 
 export const { setUserDetails } = usersSlice.actions;
 export default usersSlice.reducer;
--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
 import storage from 'redux-persist/lib/storage';
-<<<<<<< HEAD
 import users from '../reducerSlice/users'
 import logger from 'redux-logger'
 import { persistReducer, persistStore } from 'redux-persist';
@@ -26,29 +25,3 @@ import { persistReducer, persistStore } from 'redux-persist';
         export const persistor = persistStore(store)
 
 // export default store;
-=======
-import { persistReducer, persistStore } from 'redux-persist';
-import logger from 'redux-logger'
-import users from '../reducerSlice/users'
-
-const persistConfig = {
-  key: 'root',
-  storage,
-}
-
-const reducer = combineReducers({
-    users,
-    //..
-  })
-
-const persistedReducer = persistReducer(persistConfig, reducer)
-
-  export const store = configureStore({
-    reducer: persistedReducer,
-    devTools: true,
-    middleware:[logger]
-  })
-
-  export const persistor = persistStore(store)
-  
->>>>>>> d9460774f9f69c34ae1b41b8cfeec5ef3a82b3e5
