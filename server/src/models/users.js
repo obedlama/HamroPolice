f95d8bcbc07f853f0f585163f549852f3da90d2b
obedlama <obedlama6@gmail.com>
2023-08-07 17:52:06 +0545

    Added logout mechanics and profile edit

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -5,7 +5,6 @@ const userSchema = new Schema({
     fullName: String, // String is shorthand for {type: String}
     address: String,
     password: String,
-<<<<<<< HEAD
     email: String,
     phoneNumber: Number,
     image: String,
@@ -19,11 +18,3 @@ const userSchema = new Schema({
 
    module.exports = Users;
    
-=======
-    mode: {type: String, default: 'User'},
-   
-  });
-  
-  const Users = mongoose.model('Users', userSchema);
-  module.exports = Users
->>>>>>> d9460774f9f69c34ae1b41b8cfeec5ef3a82b3e5
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express=require('express')
 const router=express.Router()
 const UsersController = require('../controller/users')
@@ -8,17 +7,3 @@ router.post('/login', UsersController.loginUser )
 
 
 module.exports=router;
-=======
-const express = require('express')
-
-const router = express.Router()
-
-const userController = require('../controller/user')
-
-router.post('/register', userController.registerUser)
-router.post('/login', userController.login)
- 
-
-
-module.exports = router
->>>>>>> d9460774f9f69c34ae1b41b8cfeec5ef3a82b3e5
